2195b7288ab3a72445d31252aa8559ae91fb243b
Claudio <57859613+ClaPalo@users.noreply.github.com>
2023-06-13 00:10:48 +0200

    Fixed text position in mobiles

--- js/typing.js
+++ js/typing.js
@@ -1,16 +1,18 @@
-function myFunction() {
+function typingEffect() {
   var speed = 70 ;
-  var myName = "CLAUDIO PALOSCIA";/* The speed/duration of the effect in milliseconds */
+  var myName = "CLAUDIO PALOSCIA";
   var job = "SOFTWARE ENGINEER";
 
-  function typeWriter(txt, id, i, doPulse = false) {
-    let el = document.getElementById(id);
+  let name = document.getElementById("name");
+  let se = document.getElementById("se");
+
+  function typeWriter(txt, el, i, doPulse = false) {
     el.innerHTML = el.innerHTML.slice(0, -1);
     el.innerHTML += txt.charAt(i);
     el.innerHTML += "|";
     if (i < txt.length) {
       i++;
-      setTimeout(typeWriter, speed, txt, id, i, doPulse);
+      setTimeout(typeWriter, speed, txt, el, i, doPulse);
     } else if (doPulse) {
       el.innerHTML = el.innerHTML.slice(0, -1);
       el.innerHTML += " |";
@@ -26,11 +28,9 @@ function myFunction() {
     setTimeout(() => {el.innerHTML += " |"}, 750);
   }
 
-  typeWriter(myName, "name", 0, false);
-  typeWriter(job, "se", 0);
+  typeWriter(myName, name, 0, false);
+  typeWriter(job, se, 0, false);
 
 }
 
-window.addEventListener("load", myFunction);
-
-//TODO Fix the typing effect
+window.addEventListener("load", typingEffect);
